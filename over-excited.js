ed408fe5fa4a641448a96d2c104e6c9b1a2001f1
Wiley Walker <wiley-walker@live.com>
2017-07-28 08:31:22 -0500

    added a join()  function

--- over-excited.js
+++ over-excited.js
@@ -1,19 +1,26 @@
 var sentence = ["The", "walrus", "danced", "through", "the", "trees", "in", "the", "light", "of", "the", "moon"];
+ 
+var b = sentence.join(' ');
+
+ 
 var NewSentence = [];
 // Create an array that contains the words in the sentence
 // The addExcitement function should accept the array as the sole argument
-function addExcitement (Sentence) {
-	for (var i = 0; i < sentence.length; i++) {
-		NewSentence.push(sentence[i]);
+function addExcitement (b) {
+	for (var i = 0; i < b.length; i++) {
+		NewSentence.push(b[i]);
+		var brandNew = NewSentence.join(" ");
 
-		console.log(NewSentence);
+		console.log(brandNew);
 		
 	}
+		NewSentence.join(" - ");
+	console.log(NewSentence);
     /*
       Write a `for` loop that iterates over the array argument and
       outputs the words.
      */
 }
-addExcitement(sentence)
+addExcitement(sentence);
 
 // Invoke the function and pass in the array
